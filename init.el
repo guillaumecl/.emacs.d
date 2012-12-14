890d211cbe5b4c41b2982fb598761d6c58dcd78a
Guillaume Clément <gclement@baobob.org>
2012-12-14 16:54:24 +0100

    Modifs

--- init.el
+++ init.el
@@ -13,7 +13,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(c-default-style (quote ((awk-mode . "awk") (other . "linux"))) t)
+ '(c-default-style (quote ((awk-mode . "awk") (other . "linux"))))
  '(c-objc-method-arg-unfinished-offset 8)
  '(c-objc-method-parameter-offset 8)
  '(display-time-24hr-format t)
