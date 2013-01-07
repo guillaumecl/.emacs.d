0b068aea175c9fb00c7374f0ceea362d24ae1d11
Guillaume Clément <gclement@baobob.org>
2013-01-07 15:50:05 +0100

    Ajout du déplacement de lignes par M-<up> M-<down>

--- general.el
+++ general.el
@@ -35,3 +35,47 @@
 (global-subword-mode)
 
 (setq kill-whole-line t);; will make "Ctrl-k" kills an entire line if the cursor is at the beginning of line -- very useful.
+
+
+(defun move-line-up ()
+  (interactive)
+  (transpose-lines 1)
+  (forward-line -2))
+
+(defun move-line-down ()
+  (interactive)
+  (forward-line 1)
+  (transpose-lines 1)
+  (forward-line -1))
+
+
+(defun move-region (start end n)
+  "Move the current region up or down by N lines."
+  (interactive "r\np")
+  (let ((line-text (delete-and-extract-region start end)))
+    (forward-line n)
+    (let ((start (point)))
+      (insert line-text)
+      (setq deactivate-mark nil)
+      (set-mark start))))
+
+(defun move-region-up (start end n)
+  "Move the current line up by N lines."
+  (interactive "r\np")
+  (move-region start end (if (null n) -1 (- n))))
+
+(defun move-region-down (start end n)
+  "Move the current line down by N lines."
+  (interactive "r\np")
+  (move-region start end (if (null n) 1 n)))
+
+(defun move-line-region-up (start end n)
+  (interactive "r\np")
+  (if (region-active-p) (move-region-up start end n) (move-line-up)))
+
+(defun move-line-region-down (start end n)
+  (interactive "r\np")
+  (if (region-active-p) (move-region-down start end n) (move-line-down)))
+
+(global-set-key (kbd "M-<up>") 'move-line-region-up)
+(global-set-key (kbd "M-<down>") 'move-line-region-down)
