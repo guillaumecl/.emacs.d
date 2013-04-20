1b09104211bd4e857eee50644359497e0d406395
Guillaume Clément <gclement@baobob.org>
2013-04-20 16:32:09 +0200

    Modifs pour le nokia

--- Nokia-N900.el
+++ Nokia-N900.el
@@ -1,1 +1,8 @@
 (load "orgmode")
+
+  (custom-set-faces
+   ;; custom-set-faces was added by Custom.
+   ;; If you edit it by hand, you could mess it up, so be careful.
+   ;; Your init file should contain only one such instance.
+   ;; If there is more than one, they won't work right.
+   '(default ((t (:family "Terminus" :foundry "xos4" :slant normal :weight normal :height 162 :width normal)))))
