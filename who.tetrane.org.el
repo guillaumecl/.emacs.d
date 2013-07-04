6138f62cef8c48fbbc9567a1f97fc6d4523cbaa4
Guillaume Clément <gclement@baobob.org>
2013-07-04 15:31:26 +0200

    Various changes

--- who.tetrane.org.el
+++ who.tetrane.org.el
@@ -1,6 +1,13 @@
 (require 'lua-mode)
 (require 'cmake-mode)
 
+(add-to-list 'load-path "/usr/share/emacs/site-lisp/")
+(add-to-list 'load-path "/usr/share/emacs/site-lisp/doxymacs/")
+(add-to-list 'load-path "/usr/share/emacs/site-lisp/w3m/")
+
+(add-to-list 'load-path "~/.emacs.d/dpkg-dev-el/")
+
+
 (setq jabber-alert-info-message-hooks nil)
 (setq jabber-alert-message-hooks nil)
 
@@ -39,8 +46,6 @@
 
 ;; (ecb-activate)
 
-(select-frame (make-frame '((name . "emacs_right"))))
-
 ;(select-window (split-window-right -15))
 ;(progn
 ;  (called-interactively-p)
@@ -63,6 +68,8 @@
       (append
        '(
          ("\\.psp\\'" . python-mode)
+         ("\\CMakeLists.txt\\'" . cmake-mode)
+         ("\\.cmake\\'" . cmake-mode)
          ) auto-mode-alist))
 
 ;; (require 'fill-column-indicator)
@@ -83,5 +90,47 @@
 
 (require 'whitespace)
 (setq whitespace-line-column 120)
-(setq whitespace-style '(face empty tabs lines-tail trailing))
+(setq whitespace-style '(face lines-tail trailing))
 (global-whitespace-mode t)
+
+(require 'doxymacs)
+(add-hook 'c-mode-common-hook 'doxymacs-mode)
+(setq doxymacs-doxygen-dirs
+    '(("^/home/gclement/src/tetrane/reven/"
+       "/tmp/builds/reven/release/doc/reven.xml"
+       "file:///tmp/builds/reven/release/doc/html/"))
+)
+
+(require 'dpkg-dev-el)
+
+(setq auto-mode-alist
+      (append
+       '(
+         ("\\.dsc\\'" . debian-control-mode)
+         ) auto-mode-alist))
+
+(require 'w3m)
+
+(setq browse-url-browser-function 'ffap-w3m-other-window)
+
+
+(defun ffap-w3m-other-window (url &optional new-session)
+  "Browse url in w3m.
+  If current frame has only one window, create a new window and browse the webpage"
+  (interactive (progn
+                 (require 'browse-url)
+                 (browse-url-interactive-arg "Emacs-w3m URL: ")))
+  (let ((w3m-pop-up-windows t))
+    (if (one-window-p) (split-window))
+    (other-window 1)
+    (w3m-browse-url url new-session)))
+
+(setq w3m-use-cookies t)
+(setq w3m-pop-up-windows t)
+
+(setq w3m-coding-system 'utf-8
+      w3m-file-coding-system 'utf-8
+      w3m-file-name-coding-system 'utf-8
+      w3m-input-coding-system 'utf-8
+      w3m-output-coding-system 'utf-8
+      w3m-terminal-coding-system 'utf-8)
