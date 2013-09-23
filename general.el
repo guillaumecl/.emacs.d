d47fdbc550cf7fa0de206cbae51e6a53cd1b31cb
Guillaume Clément <gclement@baobob.org>
2013-09-23 12:08:14 +0200

    Meilleur caractère pour les lignes en console

--- general.el
+++ general.el
@@ -99,3 +99,8 @@
 
 
 (xterm-mouse-mode)
+
+;; Set symbol for the border
+(set-display-table-slot standard-display-table
+                        'vertical-border
+                        (make-glyph-code ?┃))
