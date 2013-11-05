99e42d2ba19da47aa737f8fae1448c95afc356c5
user <user@Nokia-N900.(none)>
2013-11-05 19:47:49 +0100

    modif

--- init.el
+++ init.el
@@ -55,12 +55,12 @@
 	  (require 'tramp)
 	  )
   )
-
-(load (system-name))
-
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(default ((t (:family "Terminus" :foundry "xos4" :slant normal :weight normal :height 122 :width normal)))))
+
+(load (system-name))
+
