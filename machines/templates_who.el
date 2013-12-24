d741aff131f63b6f2e4d26be193ad420a2ea48ca
Guillaume Clément <gclement@baobob.org>
2013-12-24 10:13:10 +0100

    Mise à jour des templates pour prendre le répertoire courant comme nom de module.

--- machines/templates_who.el
+++ machines/templates_who.el
@@ -1,14 +1,24 @@
+(defun find-module(path)
+  (if
+      (string-match "reven/\\([a-z]*\\)/" path)
+      (match-string 1 path)
+    "module")
+  )
+
 ;; autoinsert C/C++ header
 (define-auto-insert
-  (cons "\\.\\([Hh]\\|hh\\|hpp\\)\\'"  "My C / C++ header")
+  (cons "\\.\\([Hh]\\|hh\\|hpp\\)\\'"  "Reven C++ header")
   '(nil
 	(let* ((noext (substring buffer-file-name 0 (match-beginning 0)))
 		   (nopath (file-name-nondirectory noext))
-		   (ident (concat (upcase nopath) "_H")))
-	  (concat "#ifndef TETRANE_REVEN_" ident "\n"
-			  "#define TETRANE_REVEN_" ident  "\n\n\n"
-			  "namespace tetrane {\n"
-			  "namespace module {\n"
+		   (ident (concat (upcase nopath) "_H"))
+           (module (find-module buffer-file-name))
+           (id_module (upcase module))
+           )
+	  (concat "#ifndef TETRANE_REVEN_" id_module "_" ident "\n"
+			  "#define TETRANE_REVEN_" id_module "_" ident  "\n\n\n"
+			  "namespace reven {\n"
+			  "namespace " module " {\n"
 			  "\n\n"
 			  "//! Documentation for the new class.\n"
 			  "class " nopath "\n"
@@ -19,27 +29,29 @@
 			  "private:\n"
 			  "}; // class " nopath
 			  "\n\n"
-			  "}} // namespace tetrane::module\n"
-			  "\n\n#endif // TETRANE_REVEN_" ident "\n"))
+			  "}} // namespace reven::" module "\n"
+			  "\n\n#endif // TETRANE_REVEN_" id_module "_" ident "\n"))
   ))
 
 ;; auto insert C/C++
 (define-auto-insert
-  (cons "\\.\\([Cc]\\|cc\\|cpp\\)\\'" "My C++ implementation")
+  (cons "\\.\\([Cc]\\|cc\\|cpp\\)\\'" "Reven C++ class")
   '(nil
 	(let* ((noext (substring buffer-file-name 0 (match-beginning 0)))
 		   (nopath (file-name-nondirectory noext))
 		   (ident (concat nopath ".h")))
+           (module (find-module buffer-file-name))
+           (id_module (upcase module))
 	  (concat "#include \"" ident "\"\n"
 			  "\n\n\n"
-			  "namespace tetrane {\n"
-			  "namespace module {\n"
+			  "namespace reven {\n"
+			  "namespace " module " {\n"
 			  "\n\n\n"
 			  nopath "::" nopath "()\n"
 			  "{\n"
 			  "}\n"
 			  "\n\n\n"
-			  "}} // namespace tetrane::module\n"
+			  "}} // namespace reven::" module "\n"
 			  ))
 	))
 
