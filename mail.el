36a8f66905222023699fabd775d24468db385297
Guillaume Clement <gclement@baobob.org>
2014-02-24 20:42:59 +0100

    Début d'un support des mails non lus dans emacs

--- mail.el
+++ mail.el
@@ -13,3 +13,27 @@
                                                           (interactive)
                                                           (save-buffer)
                                                           (server-edit)))))))
+(defvar mail-mode-string
+  ""
+  "Current value of the mail mode string in the modeline.")
+
+(define-minor-mode display-mail-mode
+  "Shows the number of unread messages."
+  :global t
+
+  (setq mail-mode-string "")
+  (or global-mode-string (setq global-mode-string '("")))
+  (add-to-list 'global-mode-string 'mail-mode-string t)
+)
+
+(defun show_mail_count(count)
+  (if (= count 0)
+	  (setq mail-mode-string "")
+	(progn
+	  (setq mail-mode-string (format " %d✉" count))
+	  (message (format "Incoming mails. Currently %d are unread." count))
+	  )
+	)
+)
+
+(display-mail-mode 1)
