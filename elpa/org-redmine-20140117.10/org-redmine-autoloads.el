1daf662d0478da27cafc802052348affc4f7873e
Guillaume CLÉMENT <gclement@baobob.org>
2014-05-04 20:00:39 +0200

    Suppression de packages inutilisés

--- elpa/org-redmine-20140117.10/org-redmine-autoloads.el
+++
@@ -1,18 +0,0 @@
-;;; org-redmine-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-
-
-;;;### (autoloads nil nil ("org-redmine-pkg.el" "org-redmine.el")
-;;;;;;  (21306 57714 242660 519000))
-
-;;;***
-
-(provide 'org-redmine-autoloads)
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; coding: utf-8
-;; End:
-;;; org-redmine-autoloads.el ends here
