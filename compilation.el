81645be62e45c5a05e6a04a9b9cc058427ead129
Guillaume Clement <gclement@baobob.org>
2014-05-06 21:42:51 +0200

    Cleaner compilation code, with less duplicated code.

--- compilation.el
+++ compilation.el
@@ -1,8 +1,11 @@
 
-(defun compilation-visible ()
-  "Return t if some compilation buffer is inside a window"
-  (let ((compilation-buffer (get-buffer-create "*compilation*")))
-    (get-buffer-window compilation-buffer)))
+(defun compilation-buffer()
+  "The compilation buffer. Create one if necessary."
+  (get-buffer-create "*compilation*"))
+
+(defun compilation-window ()
+  "If some compilation buffer is inside a window, return it, otherwise nil."
+  (get-buffer-window (compilation-buffer)))
 
 (defun open-compilation ()
   "Open the compilation buffer.
@@ -10,37 +13,37 @@ If there is no compilation buffer available, create one.
 If there is already a window showing the compilation buffer, don't do anything.
 If there is no window showing the compilation buffer, show one by splitting the current frame root window vertically."
   (interactive)
-  (when (not (compilation-visible))
-    (let ((compilation-buffer (get-buffer-create "*compilation*"))
+  (when (not (compilation-window))
+    (let ((compile-buffer (compilation-buffer))
           (compile-win  (split-window (frame-root-window) -10)))
       (setq compilation-window-height 10)
       (set-window-buffer
        compile-win
-       compilation-buffer)
+       compile-buffer)
       (set-window-dedicated-p compile-win t))))
 
 (defun close-compilation ()
   "Delete all windows that shows the compilation buffer in the current frame."
   (interactive)
-  (while (compilation-visible)
-    (let ((compilation-buffer (get-buffer-create "*compilation*")))
-      (delete-window (get-buffer-window compilation-buffer)))))
+  (let ((compile-window nil))
+    (while (setq compile-window (compilation-window))
+      (delete-window compile-window))))
 
 (defun toggle-compilation ()
   "Shows the compilation window if no window is displaying the compilation buffer, otherwise close them."
   (interactive)
-  (if (compilation-visible)
+  (if (compilation-window)
       (close-compilation)
       (open-compilation)))
 
 (defadvice recompile (before setup-recompilation-buffer (&optional edit-command))
-  (when (not (compilation-visible))
+  (when (not (compilation-window))
     (open-compilation)))
 
 (ad-activate 'recompile)
 
 (defadvice compile (before setup-recompilation-buffer (command &optional comint))
-  (when (not (compilation-visible))
+  (when (not (compilation-window))
     (open-compilation)))
 
 (ad-activate 'compile)
