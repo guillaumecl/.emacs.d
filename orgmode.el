62a375a93a941d3d85c758a1458a06ba617c8436
Guillaume Clement <gclement@baobob.org>
2014-11-09 13:57:33 +0100

    Add some basic things to publish a blog with orgmode
    Signed-off-by: Guillaume Clement <gclement@baobob.org>

--- orgmode.el
+++ orgmode.el
@@ -43,3 +43,35 @@
                            (add-hook 'after-save-hook 'org-save-hook nil t)))
 (add-hook 'org-mode-hook (lambda ()
                            (add-hook 'find-file-hook 'org-save-hook nil t)))
+
+
+(setq org-publish-project-alist
+      '(
+	("baobob.org-posts"
+	 :base-directory "~/blog"
+	 :base-extension "org"
+	 :exclude "menu.org"
+	 :publishing-directory "~/public_html"
+	 :publishing-function org-publish-org-to-html
+	 :html-preamble "<div id='menu'>
+		<ul>
+			<li><a href='index.html'>Home</a>
+		</ul>
+		</div>"
+	 :html-postamble ""
+	 :recursive t
+	 :author nil
+	 :section-numbers nil
+	 :table-of-contents t
+	 :language "fr"
+	 :style "<link rel=\"stylesheet\"
+                     href=\"resources/baobob.css\"
+                     type=\"text/css\"/>")
+	("baobob.org-static"
+	 :base-directory "~/blog/"
+	 :base-extension "css\\|js\\|png\\|jpg\\|gif\\|pdf\\|mp3\\|ogg\\|swf"
+	 :publishing-directory "~/public_html/"
+	 :recursive t
+	 :publishing-function org-publish-attachment)
+	("baobob.org"
+	 :components ("baobob.org-posts" "baobob.org-static"))))
