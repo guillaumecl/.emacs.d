44a2c3b274d3981d28365de7f647e9651b4006a9
Guillaume Clement <gclement@baobob.org>
2014-11-09 13:56:33 +0100

    Stop using compile-current-project on etna for now
    This needs more tweaking to be good.
    
    Signed-off-by: Guillaume Clement <gclement@baobob.org>

--- machines/etna.baobob.org.el
+++ machines/etna.baobob.org.el
@@ -1,13 +1,10 @@
 (load "desktop")
 
-(global-set-key (kbd "<f8>") 'compile-current-project)
-(global-set-key (kbd "S-<f8>")
-		(lambda ()
-		  (interactive)
-		  (let ((temp compilation-read-command))
-		    (setq compilation-read-command t)
-		    (call-interactively 'compile)
-		    (setq compilation-read-command temp))))
+(global-set-key (kbd "<f8>") 'recompile)
+(global-set-key (kbd "<f18>") 'compile)
+(global-set-key (kbd "S-<f8>") 'compile)
+(global-set-key (kbd "<f9>") 'recompile)
+(global-set-key (kbd "<f19>") 'compile)
 (setq auto-mode-alist
       (append
        '(
