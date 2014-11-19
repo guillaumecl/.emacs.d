7ef7dd9422cdf8fc8a1c3869ca004add16e9f963
Guillaume Clément <gclement@baobob.org>
2014-11-19 17:08:33 +0100

    Use a space for separator in console mode
    This is more readable actually, and it's not a special char.
    
    Signed-off-by: Guillaume Clément <gclement@baobob.org>

--- files/console.el
+++ files/console.el
@@ -2,7 +2,7 @@
 ;; Set symbol for the border
 (set-display-table-slot standard-display-table
                         'vertical-border
-                        (make-glyph-code ?│))
+                        (make-glyph-code ? ))
 
 (global-set-key (kbd "<clearline>") 'end-of-buffer)
 
