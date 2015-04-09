b2baeeabc15052bda1a3159bfb738adf6dcdb1fe
Guillaume Clement <gclement@baobob.org>
2015-04-09 21:08:39 +0200

    F1 to get the documentation of current symbol in C modes
    Signed-off-by: Guillaume Clement <gclement@baobob.org>

--- files/qt.el
+++ files/qt.el
@@ -4,15 +4,18 @@
   (let* ((procname "qt-assistant")
          (search (symbol-at-point)))
     (if (not (process-status procname))
-        (start-process
-         procname procname
-         "assistant"
-         "-stylesheet=/home/gclement/assistant.css"
-         "-stylesheet=/home/tortuxm/assistant.css"
-         "-enableremotecontrol"))
+	(start-process
+	 procname procname
+	 "/home/tortuxm/projets/qt/bin/assistant"
+	 "-stylesheet=/home/gclement/assistant.css"
+	 "-stylesheet=/home/tortuxm/assistant.css"
+	 "-enableremotecontrol"))
     (process-send-string procname
-                         (format "activatekeyword %s;\n" search))
+                         (format "activatekeyword %s;\n" search))))
 
-    ))
+(global-set-key (kbd "<f1>") 'my-qt-assistant)
 
-(global-set-key (kbd "<f9>") 'my-qt-assistant)
+(defun qt-c-hooks ()
+  (local-set-key (kbd "<f1>") 'my-qt-assistant))
+
+(add-hook 'c-mode-common-hook 'qt-c-hooks)
